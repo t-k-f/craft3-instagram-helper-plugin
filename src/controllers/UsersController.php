d758be4277aea7d76c8402ff03212851f0b7b4dc
Jan Thoma <jan.thoma@t-k-f.ch>
2017-11-07 17:16:37 +0100

    removed unavaible endpoints logic to service class

--- src/controllers/UsersController.php
+++ src/controllers/UsersController.php
@@ -22,108 +22,31 @@ use craft\web\Controller;
  */
 class UsersController extends Controller
 {
-
-    // Protected Properties
-    // =========================================================================
-
     protected $allowAnonymous = true;
     private $apiUrl = 'https://api.instagram.com/v1/users';
     public $enableCsrfValidation = false;
-
-    // Private Methods
-    // =========================================================================
-
-    private function settings()
-    {
-        return \tkf\instagramapihelper\InstagramApiHelper::getInstance()->settings;
-    }
-
-    private function request($path)
-    {
-        $cacheKey = md5($path);
-        $cache = Craft::$app->cache->get($cacheKey);
-
-        if($cache !== false)
-        {
-            return $cache;
-        }
-        else
-        {
-            $curl = curl_init($path);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $result = json_decode(curl_exec($curl));
-            curl_close($curl);
-            Craft::$app->cache->set($cacheKey, $result, 300);
-
-            return $result;
-        }
-    }
-
-    private function response ($path)
-    {
-        $response = $this->request($path);
-
-        return $this->asJson($response);
-    }
-
-    private function checkParams ($params)
-    {
-        $post = Craft::$app->getRequest()->getBodyParams();
-        $queryString = '';
-
-        foreach ($params as $param)
-        {
-            if(isset($post[$param]))
-            {
-                $queryString .= '&' . $param . '=' . $post[$param];
-            }
-        }
-
-        return $queryString;
-    }
 
     // Public Methods
     // =========================================================================
 
     public function actionSelf()
     {
-        $path = $this->apiUrl . '/self/?access_token=' . $this->settings()['instagramApiToken'];
+        $response = InstagramApiHelper::getInstance()->helpers->getResponse($this->apiUrl, 'self', []);
 
-        return $this->response($path);
-    }
-
-    public function actionUser($userId)
-    {
-        $path = $this->apiUrl . '/' . $userId . '/?access_token=' . $this->settings()['instagramApiToken'];
-
-        return $this->response($path);
+        return $this->asJson($response);
     }
 
     public function actionSelfRecent()
     {
-        $path = $this->apiUrl . '/self/media/recent/?access_token=' . $this->settings()['instagramApiToken'] . $this->checkParams(['count', 'max_id', 'min_id']);
+        $response = InstagramApiHelper::getInstance()->helpers->getResponse($this->apiUrl, 'self/media/recent', ['count', 'max_id', 'min_id']);
 
-        return $this->response($path);
-    }
-
-    public function actionUserRecent($userId)
-    {
-        $path = $this->apiUrl . '/' . $userId . '/media/recent/?access_token=' . $this->settings()['instagramApiToken'] . $this->checkParams(['count', 'max_id', 'min_id']);
-
-        return $this->response($path);
+        return $this->asJson($response);
     }
 
     public function actionSelfLiked()
     {
-        $path = $this->apiUrl . '/self/media/liked/?access_token=' . $this->settings()['instagramApiToken'] . $this->checkParams(['count', 'max_like_id']);
+        $response = InstagramApiHelper::getInstance()->helpers->getResponse($this->apiUrl, 'self/media/liked', ['count', 'max_id', 'min_id']);
 
-        return $this->response($path);
-    }
-
-    public function actionSearch()
-    {
-        $path = $this->apiUrl . '/search?q=tkf&access_token=' . $this->settings()['instagramApiToken'];
-
-        return $this->response($path);
+        return $this->asJson($response);
     }
 }
